1bc353f5bfafd0531bf6a6742b5ba2287784a892
gxvigo <gxvigo@gmail.com>
2016-06-03 13:54:14 +1200

    customized bank, account and transaction model to hide some methods

--- common/models/account.js
+++ common/models/account.js
@@ -1,3 +1,20 @@
+
+// https://github.com/Neil-UWA/loopback-remote-routing
+// https://docs.strongloop.com/display/APIC/Exposing+models+over+REST
+var RemoteRouting = require('loopback-remote-routing');
+
 module.exports = function(Account) {
+  // use only to expose specified remote methods
+  // symbol @ denotes static method
+  // scope methods are static method
+  RemoteRouting(Account, {only: [
+      '@find',
+      '@create',
+      '@count'
+  ]});
+
+  //disable all remote methods omitting options
+
+  // RemoteRouting(Bank)
 
 };
--- common/models/transaction.js
+++ common/models/transaction.js
@@ -1,3 +1,20 @@
+
+// https://github.com/Neil-UWA/loopback-remote-routing
+// https://docs.strongloop.com/display/APIC/Exposing+models+over+REST
+var RemoteRouting = require('loopback-remote-routing');
+
 module.exports = function(Transaction) {
+  // use only to expose specified remote methods
+  // symbol @ denotes static method
+  // scope methods are static method
+  RemoteRouting(Transaction, {only: [
+      '@find',
+      '@create',
+      '@count'
+  ]});
+
+  //disable all remote methods omitting options
+
+  // RemoteRouting(Bank)
 
 };
